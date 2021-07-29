08666f3154bc7670d641d7541278365aade5dbe8
Cloutar <cloutar@outlook.com>
2021-07-29 11:33:54 +0800

    dialog

---
+++ Base/dialog/dialog/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dialog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            //注册事件
+            form2.TransfEvent += form2_TransfEvent;
+            form2.ShowDialog();
+        }
+
+        void form2_TransfEvent(String value)
+        {
+            textBox1.Text = value;
+        }
+    }
+}
---
+++ Base/dialog/dialog/Form2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dialog
+{
+    //声明委托和事件
+    public delegate void TransfDelegate(String value);
+
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        public event TransfDelegate TransfEvent;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //触发事件
+            TransfEvent(textBox1.Text);
+            this.Close();
+        }
+    }
+}
